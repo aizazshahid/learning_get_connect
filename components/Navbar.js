5f0bcd6fd3c2e42608d8e2bf936d5e50ad82c639
Aizaz <aizazshahid786@gmail.com>
2020-12-01 17:31:57 +0500

    Navbar edited

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,40 @@
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import ShareOutlined from "@material-ui/icons/ShareOutlined";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ShareOutlined from "@material-ui/icons/ShareOutlined";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+const Navbar = ({ classes, router, pageProps: { auth } }) => {
+  const { user = {} } = auth || {};
 
+  return (
+    <AppBar className={classes.appBar} position={router.pathname === '/' ? 'fixed' : 'static'}>
+      <Toolbar>
+        <ShareOutlined className={classes.icon} />
+        <Typography variant="h3" component="h1" className={classes.toolbarTitle}>
+          Get Connect
+        </Typography>
+
+        {
+          user._id ? (
+            // Auth Navigation 
+            <div>
+              <Button>Profile</Button>
+              <Button variant="outlined">Sign out</Button>
+            </div>
+          ) : (
+            <div>
+              <Button>Sign in</Button>
+              <Button>Sign up</Button>
+            </div>
+          )
+        }
+      </Toolbar>
+    </AppBar>
+  );
+  
+};
 const styles = theme => ({
   appBar: {
     // z-index 1 higher than the fixed drawer in home page to clip it under the navigation
